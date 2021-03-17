be92a5cad26d5f01e023e0c187001aa42e7e84dc
Coder7052 <80103867+Coder7052@users.noreply.github.com>
2021-03-17 14:09:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function cp(){
+    document.getElementById("ad1").play();
+    document.getElementById("img1").src="cat-meme-54-0.jpg";}
+
+
+function cu(){
+    document.getElementById("ad1").play();
+    document.getElementById("img2").src="977ac9921e58eb4d42fa2be0add0fa52.jpg";}
+    
